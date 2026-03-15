cf3c8203a43ac680a49785a682ed0a4a187b8296
hanifrosidi <hanifrosidi@gmail.com>
2026-03-15 14:25:07 +0700

    create e2e test failed

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -60,10 +60,10 @@ describe("login spec", () => {
     cy.get("input[placeholder='password']").type("DicodingOkeBGT");
 
     cy.get("button")
-      .contains(/^Masuk$/)
+      .contains(/^Login$/)
       .click();
 
-    cy.contains("Masuk").click();
+    cy.contains("Login").click();
 
     cy.url().should("not.include", "/login");
   });
